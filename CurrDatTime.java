5919d9663ba074150baf991257e138e19855db69
Shashikiran M <shashikiran.madhuranath@QUALITEST.LOCAL>
2019-01-24 11:06:32 +0530

    JenGit learning

---
+++ CurrDatTime.java
@@ -0,0 +1,12 @@
+
+
+import java.util.Date;
+
+public class CurrDatTime {
+	public static void main(String[] args) {
+	System.out.println("Hello");
+	 Date date = new Date();
+	 System.out.println(date.toString());
+	 
+	}
+}
